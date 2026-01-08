b2efed7c4c2d6d32bd9f054ae50ff6f8727bb771
DESKTOP-VJ6LN2T\ParkIntae <pit1995@outlook.kr>
2026-01-08 18:18:53 +0900

    불필요한 파일 정리

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      app: path.resolve(__dirname, './app'),
       '@': path.resolve(__dirname, './src'),
     },
   },
