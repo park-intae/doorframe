3e68ce3483fe3b74a907bcce14506413b856ed85
DESKTOP-VJ6LN2T\ParkIntae <pit1995@outlook.kr>
2026-04-09 14:18:58 +0900

    css 레이아웃 미세 조정

--- src/components/organisms/Main/MainContainer.tsx
+++ src/components/organisms/Main/MainContainer.tsx
@@ -5,10 +5,10 @@ import MainSec from "./MainSec";
 
 export default function Main() {
     return (
-        <main className="relative flex flex-1 items-stretch min-h-dvh">
+        <main className="relative flex flex-1 w-full max-w-[1920px] mx-auto items-stretch h-screen overflow-hidden">
             <Login />
             <BookmarkBar />
-            <article className="mainSec flex flex-1 flex-col justify-between items-center">
+            <article className="mainSec flex flex-1 flex-col justify-between items-center h-full overflow-hidden pl-20">
                 <MainSec />
                 <Bottom />
             </article>
