f9424396c63477d7968bb1e7c64efc92f5b57448
BOOK-GJJ3KVP6H9\pit19 <pit1995@outlook.kr>
2026-09-09 18:54:18 +0900

    style: 현재 날짜 및 시간 섹션 좌측 패딩 확대 적용

--- src/components/organisms/today/Today.tsx
+++ src/components/organisms/today/Today.tsx
@@ -16,7 +16,7 @@ export default function Today() {
     }, [dispatch]);
 
     if (!date || !time) return (
-        <div id="today" className="rounded-xl m-3 p-3 flex flex-col gap-4 animate-pulse">
+        <div id="today" className="rounded-xl m-1 py-2 pl-6 mdDT:pl-8 pr-2 flex flex-col gap-4 animate-pulse">
             <div className="h-8 bg-gray-200 rounded w-1/2"></div>
             <hr className="border-[#727272]/80" />
             <div className="h-12 bg-gray-200 rounded w-3/4"></div>
@@ -24,7 +24,7 @@ export default function Today() {
     );
 
     return (
-        <section id="today" className="font-bold rounded-xl m-1 p-2 text-left flex flex-col gap-2 smDT:gap-1 mdDT:gap-4" aria-label="오늘의 날짜와 시간">
+        <section id="today" className="font-bold rounded-xl m-1 py-2 pl-6 mdDT:pl-8 pr-2 text-left flex flex-col gap-2 smDT:gap-1 mdDT:gap-4" aria-label="오늘의 날짜와 시간">
             <div id="date" className="text-context text-xl smDT:text-lg mdDT:text-3xl">{date}</div>
             <hr className="border-[#727272]/80 border-1" />
             <time id="time" className="time text-title text-4xl smDT:text-5xl mdDT:text-7xl whitespace-nowrap">{time}</time>
